bcaf1630233cef905593aef95a762226a29d03ff
Nona MacDermid <nonamacd@gmail.com>
2015-11-05 20:37:01 -0400

    After Auths completed before Toaster

---
+++ MoneyBunny/MoneyBunny/scripts/controllers/nav.js
@@ -0,0 +1,15 @@
+// nav.js
+
+'use strict';
+
+app.controller('NavController', function($scope, $location, Auth) {
+
+	$scope.currentUser = Auth.user;
+	$scope.signedIn = Auth.signedIn;
+
+	$scope.logout = function() {
+		Auth.logout();
+		console.log("Logged Out!");
+		$location.path('/');
+	};
+});
